f113abf6bed859380034b852d57f32d3cd8e2579
BaronKimaru <bizzinali@gmail.com>
2020-01-21 14:18:24 +0300

    Updates

---
+++ precache-manifest.2ba5d671bcfd616a4e0ed7105c89869c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baec9ecd65a8815dae40222b592fbee1",
+    "url": "/ReactFoodieApp/index.html"
+  },
+  {
+    "revision": "5a785002e0483010181d",
+    "url": "/ReactFoodieApp/static/css/main.191a1013.chunk.css"
+  },
+  {
+    "revision": "dd80587af9987736034b",
+    "url": "/ReactFoodieApp/static/js/2.506593d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactFoodieApp/static/js/2.506593d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a785002e0483010181d",
+    "url": "/ReactFoodieApp/static/js/main.d2252505.chunk.js"
+  },
+  {
+    "revision": "9ee4e43afb0f61ea77d2",
+    "url": "/ReactFoodieApp/static/js/runtime-main.93fbc16a.js"
+  }
+]);
